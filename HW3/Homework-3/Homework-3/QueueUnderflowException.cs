ccc20e143c0f376dac46f4a388559c0ddb32f70d
Quincey Freeman <qfreeman19@mail.wou.edu>
2020-10-16 22:38:43 -0700

    Added and implement QueueUnderflowException.cs

---
+++ HW3/Homework-3/Homework-3/QueueUnderflowException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Homework_3
+{
+    public class QueueUnderflowException : Exception
+    {
+        public QueueUnderflowException()
+            : base()
+        {
+
+        }
+        public QueueUnderflowException(String message)
+            : base(message)
+        {
+
+        }
+    }
+}
