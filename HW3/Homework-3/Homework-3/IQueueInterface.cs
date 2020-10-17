d67ca955601f3a005fd84d4df2d54c00b1c40e89
Quincey Freeman <qfreeman19@mail.wou.edu>
2020-10-16 22:28:28 -0700

    Added IQueueInterface file and methods without throw statements

---
+++ HW3/Homework-3/Homework-3/IQueueInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Homework_3
+{
+    public interface IQueueInterface
+    {
+        T Push<T>(T element);
+
+        T Pop<T>();
+
+        T Peek<T>();
+
+        bool IsEmpty();
+    }
+}
