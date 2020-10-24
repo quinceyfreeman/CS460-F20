05e2a6ce4f01567e27ed4c93580e35e77a083d0a
quinceyfreeman <quinceyfreeman@outlook.com>
2020-10-24 01:53:08 -0700

    changed project structure (rgb and interpolator have own controllers); update rgb page

---
+++ HW4/HW4Solution/HW4Project/Controllers/RGBController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HW4Project.Models;
+
+namespace HW4Project.Controllers
+{
+    public class RGBController : Controller
+    {
+        [HttpGet]
+        public IActionResult RGBColor()
+        {
+            return View();
+        }
+    }
+}
