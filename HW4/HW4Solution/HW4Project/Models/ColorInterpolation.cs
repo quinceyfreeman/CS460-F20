e5d62a901567a639c803f52b3e69fcaaaadd6e5a
quinceyfreeman <quinceyfreeman@outlook.com>
2020-10-24 14:12:22 -0700

    added requirements for inputs

--- HW4/HW4Solution/HW4Project/Models/ColorInterpolation.cs
+++ HW4/HW4Solution/HW4Project/Models/ColorInterpolation.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HW4Project.Models
 {
     public class ColorInterpolation
     {
+        [Required]
+        [RegularExpression("#[0-9A-F]{6}")]
         public string FirstColor {get; set; }
+        [Required]
+        [RegularExpression("#[0-9A-F]{6}")]
         public string SecondColor { get; set; }
+        [Required]
         public int NumberOfColors { get; set; }
         public IEnumerable<string> ColorSet { get; set; }
 
