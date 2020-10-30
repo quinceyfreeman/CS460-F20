25066247f01743f4ccb8b63e89c3feb0586d9c52
quinceyfreeman <quinceyfreeman@outlook.com>
2020-10-29 21:54:15 -0700

    created db and models

---
+++ HW5/HW5Solution/HW5Project/Models/Assignments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HW5Project.Models
+{
+    public partial class Assignments
+    {
+        [Key]
+        [Column("ID")]
+        public long Id { get; set; }
+        public long Priority { get; set; }
+        [Required]
+        [Column(TypeName = "DATETIME")]
+        public byte[] DueDate { get; set; }
+        [Required]
+        [Column(TypeName = "NVARCHAR(10)")]
+        public string Course { get; set; }
+        [Required]
+        [Column(TypeName = "NVARCHAR(64)")]
+        public string Title { get; set; }
+        [Column(TypeName = "NVARCHAR(512)")]
+        public string Notes { get; set; }
+    }
+}
